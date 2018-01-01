8dcb2a1ca974bdf9fc7519930ffb6150665d13b5
Ruman Gerst <ruman.gerst@uni-jena.de>
2018-01-01 22:01:15 +0100

    + Added map generator

--- Map.gd
+++ Map.gd
@@ -1,19 +1,21 @@
-extends Node2D
+extends Control
+
+var tile_size = 32
 
 var map_width = 0
 var map_height = 0
 var map_tiles = []
 
 func _ready():
-	initialize_map(5, 5)
-	add_tile("end", 0, 0, 0)
-	add_tile("straight", 0, 1, 0)
-	add_tile("straight", 0, 2, 0)
-	add_tile("tri", 0, 3, 0)
-	add_tile("end", 0, 4, 0)
-	add_tile("end", 0, 3, 1)
+	randomize()
+	initialize_map(10, 10)
+	map_generator(4, 15, 4)
 	
 func initialize_map(width, height):
+	
+	# Remove existing tiles
+	for child in map_tiles:
+		child.queue_free()
 	
 	map_tiles = []
 	for i in range(width * height):
@@ -22,26 +24,117 @@ func initialize_map(width, height):
 	map_width = width
 	map_height = height
 	
+func map_generator(generator_lines, min_line_length, max_bouncing):
+	
+	var generated_lines = 0
+	
+	while generated_lines != generator_lines:
+		
+		var x = randi() % map_width
+		var y = randi() % map_height
+		
+		if(get_tile(x,y) != null):
+			continue
+		
+		var generator_pre = []		
+		for i in range(map_width * map_height):
+			generator_pre.append(0)
+			
+		# Find some path that does not disturb already generated tiles
+		var dirUpRight = randi() % 2 == 1
+		var dirInvert = 1 if randf() < 0.5 else -1		
+		
+		# Count how long the generated line will be
+		var line_length = 0
+		var bounce_count = 0
+		 
+		while true:			
+			if randf() < 0.5:
+				dirUpRight = randi() % 2 == 1
+				dirInvert = 1 if randf() < 0.5 else -1
+				
+			var dirX = dirInvert if dirUpRight else 0
+			var dirY = dirInvert if not dirUpRight else 0	
+				
+			x += dirX
+			y += dirY
+			
+			if(get_tile(x,y) != null or x < 0 or x >= map_width or y < 0 or y >= map_height):
+				if bounce_count < max_bouncing:
+					x -= dirX
+					y -= dirY
+					bounce_count += 1
+				else:
+					break
+							
+			generator_pre[x + y * map_width] = 1
+			line_length += 1
+		
+		# The map must be at least given length (but to prevent deadlock, only require for first line)
+		if(line_length < min_line_length and generator_lines == 0):
+			generator_lines -= 1
+			continue
+			
+		# Generate the map by simple connection
+		for i in range(map_width):
+			for j in range(map_height):
+				
+				var current = generator_pre[i + j * map_width]
+				
+				if current > 0:
+					
+					var north = generator_pre[i + (j - 1) * map_width] if j - 1 >= 0 else 0
+					var south = generator_pre[i + (j + 1) * map_width] if j + 1 < map_height else 0
+					var east = generator_pre[(i + 1) + j * map_width] if i + 1 < map_width else 0
+					var west = generator_pre[(i - 1) + j * map_width] if i - 1 >= 0 else 0
+					
+					var count = north + south + east + west
+					
+					if count == 1:
+						add_tile("end", 0, i, j)
+					elif count == 2 and north == south:
+						add_tile("straight", 0, i, j)
+					elif count == 2 and north != south:
+						add_tile("curve", 0, i, j)
+					elif count == 3:
+						add_tile("tri", 0, i, j)
+					elif count == 4:
+						add_tile("cross", 0, i, j)
+					else:
+						print("Empty line generated")
+						
+				
+		generated_lines += 1
+	
+	
 func add_tile(tile_type, tile_rotation, x, y):
 	
+	remove_tile(x,y)
 	print("Adding tile of type " + tile_type + " with rotation " + str(tile_rotation) + " to " + str(x) + ", " + str(y))
 	
 	var tile = load("Tile.tscn").instance()
 	map_tiles[x + y * map_width] = tile
 	tile.set_tile_type(tile_type)
 	tile.set_tile_rotation(tile_rotation)
-	tile.rect_size = Vector2(10, 10)
-	tile.rect_pivot_offset = Vector2(5, 5)
-	tile.rect_position = Vector2(x * 10, y * 10)
+	tile.rect_size = Vector2(tile_size, tile_size)
+	tile.rect_pivot_offset = Vector2(tile_size / 2.0, tile_size / 2.0)
+	tile.rect_position = Vector2(x * tile_size, y * tile_size) + rect_size / 2.0 - Vector2(map_width * tile_size, map_height * tile_size) / 2.0
 	tile.set_name("tile" + str(x) + "_" + str(y))
 	
 	add_child(tile)
 	tile.connect("updated_tile_rotation", self, "check_if_solved")
 	
+	
 func get_tile(x,y):
 	if(x < 0 or x >= map_width or y < 0 or y >= map_height):
 		return null
 	return map_tiles[x + y * map_width]
+	
+func remove_tile(x,y):
+	
+	var tile = get_tile(x,y)
+	if(tile != null):
+		tile.queue_free()
 	
 func get_tile_connection_from(x, y, direction):	
 	
