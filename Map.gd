481c6f19874fa7ee500d734bb13b995bf34fd1ea
Ruman Gerst <ruman.gerst@uni-jena.de>
2018-01-02 01:03:02 +0100

    * stuff

--- Map.gd
+++ Map.gd
@@ -1,6 +1,7 @@
 extends Control
 
-var tile_size = 32
+const MapGenerator_Random = preload("res://map_generators/random.gd")
+const MapGenerator_RandomSymm = preload("res://map_generators/random_symm.gd")
 
 var map_width = 0
 var map_height = 0
@@ -8,13 +9,20 @@ var map_tiles = []
 
 func _ready():
 	randomize()
-	#initialize_map(10, 10)
-	#map_generator(3, 8, 4)
+	initialize_map(11, 11)
+	MapGenerator_Random.map_generator_nosolution(self, 3, 8, 4)
 	
-	initialize_map(2, 1)
-	map_generator_nosolution(1, 2, 4)
+	#initialize_map(2, 1)
+	#MapGenerator_Random.map_generator_nosolution(self, 1, 2, 4)
 	
 	#deserialize("current.level")
+	
+	get_viewport().connect("size_changed", self, "update_map")
+
+func update_map():
+	for x in range(map_width):
+		for y in range(map_height):
+			update_tile_position(x, y)
 	
 func initialize_map(width, height):
 	
@@ -36,97 +44,17 @@ func clear_map():
 			map_tiles[i] = null
 			
 		
-func map_generator_nosolution(min_generated_lines, min_line_length, max_bouncing):
+
+func update_tile_position(x,y):
 	
-	map_generator(min_generated_lines, min_line_length, max_bouncing)
+	var tile = get_tile(x,y)
+	var tile_size = round(min(128, min(rect_size.x / map_width, rect_size.y / map_height)))
 	
-	while is_solved():
-		clear_map() 
-		map_generator(min_generated_lines, min_line_length, max_bouncing)
 	
-func map_generator(min_generated_lines, min_line_length, max_bouncing):
-	
-	var generated_lines = 0
-	
-	while generated_lines < min_generated_lines:
-		
-		var x = randi() % map_width
-		var y = randi() % map_height
-		
-		if(get_tile(x,y) != null):
-			continue
-		
-		var generator_pre = []		
-		for i in range(map_width * map_height):
-			generator_pre.append(0)
-			
-		# Find some path that does not disturb already generated tiles
-		var dirUpRight = randi() % 2 == 1
-		var dirInvert = 1 if randf() < 0.5 else -1		
-		
-		# Count how long the generated line will be
-		var line_length = 0
-		var bounce_count = 0
-		 
-		while true:			
-			if randf() < 0.5:
-				dirUpRight = randi() % 2 == 1
-				dirInvert = 1 if randf() < 0.5 else -1
-				
-			var dirX = dirInvert if dirUpRight else 0
-			var dirY = dirInvert if not dirUpRight else 0	
-				
-			x += dirX
-			y += dirY
-			
-			if(get_tile(x,y) != null or x < 0 or x >= map_width or y < 0 or y >= map_height):
-				if bounce_count < max_bouncing:
-					x -= dirX
-					y -= dirY
-					bounce_count += 1
-				else:
-					break
-							
-			generator_pre[x + y * map_width] = 1
-			line_length += 1
-		
-		# The map must be at least given length (but to prevent deadlock, only require for first line)
-		if(line_length < min_line_length and generated_lines == 0):
-			generated_lines -= 1
-			continue
-			
-		# Generate the map by simple connection
-		for i in range(map_width):
-			for j in range(map_height):
-				
-				var current = generator_pre[i + j * map_width]
-				
-				if current > 0:
-					
-					var north = generator_pre[i + (j - 1) * map_width] if j - 1 >= 0 else 0
-					var south = generator_pre[i + (j + 1) * map_width] if j + 1 < map_height else 0
-					var east = generator_pre[(i + 1) + j * map_width] if i + 1 < map_width else 0
-					var west = generator_pre[(i - 1) + j * map_width] if i - 1 >= 0 else 0
-					
-					var count = north + south + east + west
-					
-					var random_rotation = randi() % 4
-					
-					if count == 1:
-						add_tile("end", random_rotation, i, j)
-					elif count == 2 and north == south:
-						add_tile("straight", random_rotation, i, j)
-					elif count == 2 and north != south:
-						add_tile("curve", random_rotation, i, j)
-					elif count == 3:
-						add_tile("tri", random_rotation, i, j)
-					elif count == 4:
-						add_tile("cross", random_rotation, i, j)
-					else:
-						print("Empty line generated")
-						
-				
-		generated_lines += 1
+	if(tile != null):
+		tile.rect_size = Vector2(tile_size, tile_size)
+		tile.rect_pivot_offset = Vector2(tile_size / 2.0, tile_size / 2.0)
+		tile.rect_position = Vector2(x * tile_size, y * tile_size) + rect_size / 2.0 - Vector2(map_width * tile_size, map_height * tile_size) / 2.0
 	
 	
 func add_tile(tile_type, tile_rotation, x, y):
@@ -138,10 +66,8 @@ func add_tile(tile_type, tile_rotation, x, y):
 	map_tiles[x + y * map_width] = tile
 	tile.set_tile_type(tile_type)
 	tile.set_tile_rotation(tile_rotation)
-	tile.rect_size = Vector2(tile_size, tile_size)
-	tile.rect_pivot_offset = Vector2(tile_size / 2.0, tile_size / 2.0)
-	tile.rect_position = Vector2(x * tile_size, y * tile_size) + rect_size / 2.0 - Vector2(map_width * tile_size, map_height * tile_size) / 2.0
 	tile.set_name("tile" + str(x) + "_" + str(y))
+	update_tile_position(x, y)
 	
 	add_child(tile)
 	tile.connect("updated_tile_rotation", self, "check_if_solved")
